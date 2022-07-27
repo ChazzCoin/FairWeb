118fc0bab444b9e784ff96ab4fca9a338597d361
Chazzcoin <chazzcoin@gmail.com>
2022-07-27 13:16:52 -0500

    automated changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ current = os.getcwd()
 
 setup(
     name='FairWEB',
-    version='3.2.1',
+    version='3.2.2',
     description='Full HTML WebPage Downloader, Parser and Data Extractor. Web Crawler Included.',
     url='https://github.com/chazzcoin/fairweb',
     author='ChazzCoin',
@@ -15,7 +15,7 @@ setup(
     package_data={
         'Resources': ['*.txt']
     },
-    install_requires=['FairNLP>=1.5.1', 'FairMongo>=3.0.0', 'FairResources>=2.0.0',
+    install_requires=['FairNLP>=1.5.1', 'FairMongo>=3.1.0', 'FairResources>=2.0.0',
                       'beautifulsoup4==4.9.3', 'newspaper3k==0.2.8', 'pandas==1.4.2'],
     classifiers=[
         'Development Status :: 1 - Planning',
